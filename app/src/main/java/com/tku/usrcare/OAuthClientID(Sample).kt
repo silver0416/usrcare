386b9aa123261620643a0fadcb3d8d2720acdc5c
Silver_Hsiao <silver041692@gmail.com>
2023-11-19 16:07:04 +0800

    feat: 新增ClientID存放處說明

---
+++ app/src/main/java/com/tku/usrcare/OAuthClientID(Sample).kt
@@ -0,0 +1,11 @@
+package com.tku.usrcare
+
+
+// 修改此處的 Google Client ID 與 Line Client ID
+// 並將檔名改為 OAuthClientID.kt、object OAuthClientIDSample 改為object OAuthClientID 即可
+
+
+object OAuthClientIDSample{
+    const val GOOGLE_CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID_HERE"
+    const val LINE_CLIENT_ID = "YOUR_LINE_CLIENT_ID_HERE"
+}
